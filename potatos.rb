f38d121011f396a56caa9d9405171a364387c324
Lógica <Lgica>
2016-07-31 10:34:55 -0300

    primeiro commit

---
+++ potatos.rb
@@ -0,0 +1,9 @@
+# Não devia ter ido dormir duas da manhã
+
+5.times do 
+	print "Nós *amamos* o Ruby -- ele é fantástico!"
+end
+
+15.times do
+	puts "Não estou mais com fome, só com sono. Acho que voltei pra escola."
+end
