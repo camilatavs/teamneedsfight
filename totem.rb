0ee1d0ec5190a549adda34ec88e35be4a72d3fe9
camilatavs <camilatavs@gmail.com>
2016-08-07 10:33:01 -0300

    add anotacoes, new files

--- totem.rb
+++ totem.rb
@@ -1,1 +1,28 @@
-totem
+valor = 
+valorAtual = 
+
+função imprimir ()
+	impressora.start
+end
+
+função ExibirValor (valorAtual)
+	painel.exibir(valorAtual)
+end
+
+função novaSenha (valor)
+	valorAtual = val+1
+	imprimir ()
+end
+
+função Proximo ( )
+	Se (valorAtual > valor)
+		valor = valor + 1
+		ExibirValor (valor)
+		lembrar ()
+	end
+end
+
+
+função lembrar ()
+	tocar ()
+end
